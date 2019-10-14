7ac281994802efefe4b91950247ca5a59f938d0c
Don Aguinaldo <donald.aguinaldo@gmail.com>
2019-10-13 19:03:27 -0500

    created server tcp connection

---
+++ submission2/server.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <string>
+#include <string.h>
+#include <arpa/inet.h>
+
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  
+  // create socket
+  int listening = socket(AF_INET, SOCK_STREAM, 0);
+
+  if(listening == -1) {
+    cerr << "Cannot create socket!" << endl;
+    return -1;
+  }
+
+  // bind the socket to ip/port
+  sockaddr_in hint;
+  hint.sin_family = AF_INET;
+  hint.sin_port = htons(54000);
+  inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr); // 127.0.0.1
+
+  if(bind(listening, (sockaddr *)&hint, sizeof(hint)) == -1) {
+    cerr << "Can't bind to port/ip!" << endl;
+    return -2;
+  }
+
+  // tell socket to listen
+  if(listen(listening, SOMAXCONN) == -1) {
+    cerr << "Error trying to listen!" << endl;
+    return -3;
+  }
+
+  // accept call
+  sockaddr_in client;
+  socklen_t clientSize;
+  char host[NI_MAXHOST];
+  char svc[NI_MAXSERV];
+
+  int clientSocket = accept(listening, (sockaddr *)&client, &clientSize);
+
+  if(clientSocket == -1) {
+    cerr << "Error connecting to client!" << endl;
+    return -4;
+  }
+
+  // close listening socket
+  close(listening);
+
+  memset(host, 0, NI_MAXHOST);
+  memset(svc, 0, NI_MAXSERV);
+
+  int result = getnameinfo((sockaddr *)&client, sizeof(client), host, NI_MAXHOST, svc, NI_MAXSERV, 0);
+
+  if(result) {
+    cout << host << " connected on " << svc << endl;
+  } else {
+    inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+    cout << host << " connected on " << ntohs(client.sin_port) << endl;
+  }
+
+  // while listening - display message
+  char buf[4096];
+  while(true) {
+    memset(buf, 0, 4096);
+
+    int bytesRecv = recv(clientSocket, buf, 4096, 0);
+    
+    if(bytesRecv == -1) {
+      cerr << "Error - There was an issue connecting!" << endl;
+      break;
+    }
+
+    if(bytesRecv == 0) {
+      cout << "The client disconnected!" << endl;
+      break;
+    }
+
+    cout << "Received: " << string(buf, 0, bytesRecv) << endl;
+
+    send(clientSocket, buf, bytesRecv + 1, 0);
+  }
+
+  close(clientSocket);
+
+  return 0;
+}
